2c3c908217944fee3f0386e380fa9af613208bc4
Neo Ho <neofelisho@gmail.com>
2018-08-22 23:49:52 +0800

    Re-write the io library.

--- index.js
+++ index.js
@@ -13,66 +13,35 @@ const Detector = require('./lib/detector')
 const fs = require('fs')
 // const cheerio = require('cheerio')
 const streamToString = require('./lib/io/stream_to_string')
-const objectToStream = require('./lib/io/object_to_stream')
+const stringToStream = require('./lib/io/string_to_stream')
 
 /**
  * Module exports.
  */
+
+// let customConfigs = [
+//   {
+//     'root': '',
+//     'tag': 'link',
+//     'attributeName': 'rel',
+//     'attributeValue': '',
+//     'attributeType': 'with',
+//     'minimum': 0,
+//     'maximum': null
+//   }
+// ]
+
+// let detector = new Detector(customConfigs)
+// detector.getResult(stream).then
+// detector.getResult(stream, console.log)
+
 let path = './test/detector/test5.html'
-let url = 'https://neofelisho.github.io/2018/08/2018-08-05-nodejs-getting-started/'
+// let url = 'https://neofelisho.github.io/2018/08/2018-08-05-nodejs-getting-started/'
 // let data = fs.readFileSync(path).toString()
 let stream = fs.createReadStream(path, { encoding: 'utf8' })
-let customConfigs = [
-  {
-    'root': '',
-    'tag': 'link',
-    'attributeName': 'rel',
-    'attributeValue': '',
-    'attributeType': 'with',
-    'minimum': 0,
-    'maximum': null
-  }
-]
-
-let detector = new Detector(customConfigs)
-// detector.getResult(stream).then
-detector.getResult(stream).then(resultStream => {
-  streamToString(resultStream, data => {
-    console.log(data)
-  })
+let p1 = streamToString(stream)
+let p2 = p1.then(stringToStream)
+Promise.all([p1, p2]).then(results => {
+  // console.log(results[0])
+  results[1].pipe(process.stdout)
 })
-// let resultstream = detector.getResultByStream(stream)
-// streamToString(resultstream, data => {
-//   console.log(data)
-// })
-// detector.getResult(stream).then(results => {
-//   results.forEach(result => { console.log(result) })
-// })
-
-// detector.getResultFromStream(stream, customConfigs).then(results => {
-//   results.forEach(result => {
-//     console.log(result)
-//   })
-// })
-
-// streamToString(stream, data => {
-//   console.log(data)
-//   let results = detector.getResult(data)
-//   results.forEach(result => {
-//     console.log(result)
-//   })
-// })
-
-// let promise = new Promise((resolve) => {
-//   streamToString(stream, data => {
-//     resolve(detector.getResult(data))
-//   })
-// })
-
-// promise.then(results => {
-//   results.forEach(result => {
-//     console.log(result)
-//   })
-// })
-
-// results.forEach(result => { console.log(result) })
