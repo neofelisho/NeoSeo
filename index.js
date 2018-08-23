621895a334f8a7541a8fd1cf640fe21044ffb683
Neo Ho <neofelisho@gmail.com>
2018-08-23 23:11:45 +0800

    Resolve the problem of promises chain, need to speed up to finish this project.

--- index.js
+++ index.js
@@ -19,19 +19,19 @@ const stringToStream = require('./lib/io/string_to_stream')
  * Module exports.
  */
 
-// let customConfigs = [
-//   {
-//     'root': '',
-//     'tag': 'link',
-//     'attributeName': 'rel',
-//     'attributeValue': '',
-//     'attributeType': 'with',
-//     'minimum': 0,
-//     'maximum': null
-//   }
-// ]
+let customConfigs = [
+  {
+    'root': '',
+    'tag': 'link',
+    'attributeName': 'rel',
+    'attributeValue': '',
+    'attributeType': 'with',
+    'minimum': 0,
+    'maximum': null
+  }
+]
 
-// let detector = new Detector(customConfigs)
+let detector = new Detector(customConfigs)
 // detector.getResult(stream).then
 // detector.getResult(stream, console.log)
 
@@ -39,9 +39,17 @@ let path = './test/detector/test5.html'
 // let url = 'https://neofelisho.github.io/2018/08/2018-08-05-nodejs-getting-started/'
 // let data = fs.readFileSync(path).toString()
 let stream = fs.createReadStream(path, { encoding: 'utf8' })
-let p1 = streamToString(stream)
-let p2 = p1.then(stringToStream)
-Promise.all([p1, p2]).then(results => {
-  // console.log(results[0])
-  results[1].pipe(process.stdout)
+detector.getResult(stream, process.stdout, (err, result) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(result)
+  }
 })
+
+// let p1 = streamToString(stream)
+// let p2 = p1.then(stringToStream)
+// Promise.all([p1, p2]).then(results => {
+//   // console.log(results[0])
+//   results[1].pipe(process.stdout)
+// })
