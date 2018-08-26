3631dbd5c8fcc28f2da9311628eebac477e1e3c4
Neo Ho <neofelisho@gmail.com>
2018-08-26 22:11:20 +0800

    Add unit test for `detector`.

---
+++ test/detector.test.js
@@ -0,0 +1,30 @@
+const chai = require('chai')
+const os = require('os')
+const fs = require('fs')
+const Detector = require('../lib/detector')
+const fileToString = require('../lib/io/file_to_string')
+
+var expect = chai.expect
+
+let detector = new Detector()
+let inputFile = './test/testdata.html'
+let outputFile = './test/output_file.txt'
+let expected = [
+  '<a /> tag without rel attribute: There are 52 <a> without attribute rel in this HTML, more than 0.',
+  'No <meta> tag with name="description" attribute in <haed>: There are 0 <meta> with attribute name="descriptions" in <head>, less than 1.',
+  'No <meta> tag with name="keywords" attribute in <haed>: There are 0 <meta> with attribute name="keywords" in <head>, less than 1.'
+]
+
+describe('Test for detector with default configs', () => {
+  it('Input a html file, and output result as file', () => {
+    detector.getResult(inputFile, outputFile, (err, result) => {
+      if (err)console.log(err)
+      console.log(result)
+    })
+    fileToString(outputFile, err => { if (err) console.log(err) })
+      .then(data => {
+        let actual = data.split(os.EOL).filter(r => r !== '')
+        expect(actual).to.be.eql(expected)
+      })
+  })
+})
